5f46e7af36d3352efb8dc1594ae749fcd1df686d
GoldenXLence <jack@koskie.ca>
2022-06-12 07:42:47 -0400

    chore: remove .env from github

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ import getArrivalInfo from './getArrivalInfo'
 import dotenv from 'dotenv'
 dotenv.config()
 
+if (!process.env.AVWX_TOKEN || !process.env.HOPPIE_LOGON || !process.env.CALLSIGN) {
+    throw new Error("Missing environment variables")
+}
+
 import cron from './cron'
 cron()
 
