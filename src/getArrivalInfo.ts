8a7f266b4f22e0ed2c7b6941fc6e7ec15fbd430c
Ryan Breaker <ryan@breaker.rocks>
2022-06-13 21:41:34 -0500

    chore: implement eslint and prettier, refactor to satisfy rules

--- src/getArrivalInfo.ts
+++ src/getArrivalInfo.ts
@@ -1,54 +1,64 @@
 import gates from './data/gates'
-import axios from 'axios'
+
+interface Gate {
+  icao: string
+  gate_number: string
+  international: boolean
+}
 
 /**
- * Retreives a valid gate for the given airport and international status
+ * Retrieves a valid gate for the given airport and international status
  * @param airport 4 Letter ICAO airport code
  * @param international If the flight is international or not
  * @returns Gate object or null if not found
  */
-const getGate = (airport: String, international: Boolean) => {
-    if (airport.length != 4) {
-        throw new Error('Invalid airport code')
+const getGate = (airport: string, international: boolean): Gate | null => {
+  if (airport.length !== 4) {
+    throw new Error('Invalid airport code')
+  }
+
+  let possibleGates: Gate[] = gates.filter(
+    (gate) => gate.icao === airport && gate.international === international
+  )
+
+  if (possibleGates.length === 0) {
+    possibleGates = gates.filter((gate) => gate.icao === airport)
+
+    if (possibleGates.length === 0) {
+      return null
     }
+  }
 
-    let posibleGates = gates.filter(gate => gate.icao === airport && gate.international === international)
-
-    if (posibleGates.length == 0) {
-        posibleGates = gates.filter(gate => gate.icao === airport)
-
-        if (posibleGates.length == 0) {
-            return null
-        }
-    }
-
-    let gate = posibleGates[Math.floor(Math.random() * posibleGates.length)]
-
-    return gate || null
+  return possibleGates[Math.floor(Math.random() * possibleGates.length)] ?? null
 }
 
-export default async (flightInfo: { arr: String; dep: string; callsign: any }, eta: string | number = 'Unknown') => {
-    let date = new Date(Date.now())
-    let text = []
+const getArrivalInfo = (flightInfo: {
+  arr: string
+  dep: string
+  callsign: string
+}) => {
+  const gate = getGate(
+    flightInfo.arr,
+    !flightInfo.dep.toUpperCase().startsWith('K')
+  )
 
-    const gate = getGate(flightInfo.arr, flightInfo.dep.toUpperCase().startsWith('K') ? true : false)
-    // const arrivalWeather = await axios.get(`https://avwx.rest/api/metar/${flightInfo.arr}?`, {
-    //     headers: {
-    //         Authorization: `Bearer ${process.env.AVWX_KEY}`
-    //     }
-    // })
+  // const arrivalWeather = await axios.get(`https://avwx.rest/api/metar/${flightInfo.arr}?`, {
+  //     headers: {
+  //         Authorization: `Bearer ${process.env.AVWX_KEY}`
+  //     }
+  // })
 
-    text.push(
-        `***AUTOMATED UPLINK***`,
-        `GATE ASSIGNMENT FOR`,
-        `FLIGHT ${flightInfo.callsign}`,
-        `ARRIVING ${flightInfo.arr} IS ${gate?.gate_number}`,
-        `GROUND POWER: YES`,
-        `GROUND AIR: YES`,
-        `OPS FREQ: UNKN`,
-        `MESSAGE: KEEP APU`,
-        `SHUTDOWN WHEN ABLE`
-    )
-
-    return text
+  return [
+    `***AUTOMATED UPLINK***`,
+    `GATE ASSIGNMENT FOR`,
+    `FLIGHT ${flightInfo.callsign}`,
+    `ARRIVING ${flightInfo.arr} IS ${gate?.gate_number ?? 'UNKN'}`,
+    `GROUND POWER: YES`,
+    `GROUND AIR: YES`,
+    `OPS FREQ: UNKN`,
+    `MESSAGE: KEEP APU`,
+    `SHUTDOWN WHEN ABLE`,
+  ]
 }
+
+export default getArrivalInfo
--- src/index.ts
+++ src/index.ts
@@ -1,96 +1,83 @@
 import express from 'express'
 import fs from 'fs'
 import path from 'path'
+import dotenv from 'dotenv'
+
 import routes from './data/routes'
 import getArrivalInfo from './getArrivalInfo'
 
-import dotenv from 'dotenv'
 dotenv.config()
 
-if (!process.env.AVWX_TOKEN || !process.env.HOPPIE_LOGON || !process.env.CALLSIGN) {
-    throw new Error("Missing environment variables")
+if (
+  !process.env.AVWX_TOKEN ||
+  !process.env.HOPPIE_LOGON ||
+  !process.env.CALLSIGN
+) {
+  throw new Error('Missing environment variables')
 }
 
 import cron from './cron'
+
 cron()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT ?? 3000
 
-const app = express();
+const app = express()
 
 app.all('/', (req, res) => {
-    res
-        .status(200)
-        .json({
-            message: 'Server Online'
-        })
+  res.status(200).json({
+    message: 'Server Online',
+  })
 })
 
 app.get('/dispatch/', (req, res) => {
-    res
-        .status(400)
-        .json({
-            message: 'Bad Request, no file specified'
-        })
+  res.status(400).json({
+    message: 'Bad Request, no file specified',
+  })
 })
 
-app.get('/dispatch/:file', async (req, res) => {
-    const file = String(req.params.file)
+app.get('/dispatch/:flightFile', (req, res) => {
+  const flightFile = String(req.params.flightFile).toUpperCase().split('.')
 
-    if (file.split('.').length != 2) {
-        return res
-            .status(400)
-            .json({
-                message: 'Bad Request, file must be a valid file'
-            })
+  if (flightFile.length !== 2) {
+    return res.status(400).json({
+      message: 'Bad Request, file must be a valid file',
+    })
+  }
+
+  const flight = flightFile[0]
+  const requestType = flightFile[1]
+  const filePath = path.join(__dirname, './files/', flightFile.join('.'))
+  const flightInfo = routes.find((route) => route.callsign === flight)
+
+  const text: string[] = []
+  if (!flightInfo) {
+    text.push(`Flight ${flight} not found`)
+  } else {
+    switch (requestType) {
+      case 'TST': {
+        text.push('Test Successful')
+        break
+      }
+
+      case 'ARV': {
+        text.push(...getArrivalInfo(flightInfo))
+      }
     }
+  }
 
-    const flight = file.split('.')[0].toUpperCase()
-    const requestType = file.split('.')[1].toUpperCase()
-    const filePath = path.join(__dirname, './files/', file)
-    const flightInfo = routes.find(route => route.callsign === flight)
-    let text = []
+  text.push(process.env.FOOTER ?? '')
 
-    if (!flightInfo) {
-        text.push(`Flight ${flight} not found`)
-    } else {
-
-        switch (requestType) {
-            case 'TST': {
-                text.push('Test Successful')
-                break;
-            }
-
-            case 'ARV': {
-                const arrivalText = await getArrivalInfo(flightInfo)
-                arrivalText.forEach(line => {
-                    text.push(line)
-                });
-            }
-        }
-
-    }
-
-    text.push(process.env.FOOTER || '')
-
-    for (let i = 0; i < text.length; i++) {
-        text[i] = text[i].toUpperCase()
-    }
-
-    fs.writeFileSync(filePath, text.join('\n'), { encoding: 'utf8', flag: 'w' })
-    res
-        .status(200)
-        .sendFile(filePath)
+  fs.writeFileSync(filePath, text.join('\n').toUpperCase())
+  res.status(200).sendFile(filePath)
 })
 
 app.all('*', (req, res) => {
-    res
-        .status(404)
-        .json({
-            message: 'Not found'
-        })
+  res.status(404).json({
+    message: 'Not found',
+  })
 })
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 })
